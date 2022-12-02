f9f657686e5433fd965de0e91dbc659d893276e2
mgdeveloper95@gmail.com <mgdeveloper95@gmail.com>
2022-12-02 10:56:18 +0000

    Routes Setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AboutUs from './pages/AboutUs';
+import Contacts from './pages/Contacts';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 function App() {
-	return <>hello world!</>;
+	return (
+		<>
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<AboutUs />} />
+					<Route path="/contacts" element={<Contacts />} />
+				</Routes>
+				<Footer />
+			</Router>
+		</>
+	);
 }
 
 export default App;

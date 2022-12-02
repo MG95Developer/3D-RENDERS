7aedb3e6e7265935455fbf7b6c1050f496495abc
mgdeveloper95@gmail.com <mgdeveloper95@gmail.com>
2022-12-02 11:12:29 +0000

    JSON file content added

---
+++ src/records.js
@@ -0,0 +1,78 @@
+import IMG1 from '../src/images/1.jpeg';
+import IMG2 from '../src/images/2.jpeg';
+import IMG3 from '../src/images/3.jpeg';
+import IMG4 from '../src/images/4.jpeg';
+import IMG5 from '../src/images/5.jpeg';
+import IMG6 from '../src/images/6.jpeg';
+import IMG7 from '../src/images/7.jpeg';
+import IMG8 from '../src/images/8.jpeg';
+import IMG9 from '../src/images/9.jpeg';
+import IMG10 from '../src/images/10.jpg';
+import IMG11 from '../src/images/11.jpg';
+import IMG12 from '../src/images/12.jpg';
+
+let data = [
+	{
+		id: 1,
+		imgSrc: IMG1,
+		project_type: 'interior',
+	},
+	{
+		id: 2,
+		imgSrc: IMG2,
+		project_type: 'interior',
+	},
+	{
+		id: 3,
+		imgSrc: IMG3,
+		project_type: 'interior',
+	},
+	{
+		id: 4,
+		imgSrc: IMG4,
+		project_type: 'interior',
+	},
+	{
+		id: 5,
+		imgSrc: IMG5,
+		project_type: 'exterior',
+	},
+
+	{
+		id: 6,
+		imgSrc: IMG6,
+		project_type: 'interior',
+	},
+	{
+		id: 7,
+		imgSrc: IMG7,
+		project_type: 'interior',
+	},
+	{
+		id: 8,
+		imgSrc: IMG8,
+		project_type: 'interior',
+	},
+	{
+		id: 9,
+		imgSrc: IMG9,
+		project_type: 'interior',
+	},
+	{
+		id: 10,
+		imgSrc: IMG10,
+		project_type: 'exterior',
+	},
+	{
+		id: 11,
+		imgSrc: IMG11,
+		project_type: 'interior',
+	},
+	{
+		id: 12,
+		imgSrc: IMG12,
+		project_type: 'exterior',
+	},
+];
+
+export default data;
